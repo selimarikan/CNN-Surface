1793874fdada4dd186022b7549edef6271eaec6d
tyathalae <selimarikan@gmail.com>
2017-05-23 12:19:01 +0200

    Added augmentation flag to data creation, converted image scaling method to bicubic

--- torch_datacreate.lua
+++ torch_datacreate.lua
@@ -4,51 +4,73 @@ require 'image'
 require 'nn'
 require 'dataset' -- local
 
-local basePath = 'G:/Selim/Thesis/Code/3MSet_All/'
+local basePath = 'G:/Selim/Thesis/Code/EBA5/'
 local extension = 'png'
 local imageSize = 64
+local setName = 'EBA5All' .. tostring(imageSize)
+local augmentation
 
 -- Load images works, loads a table of tensors
 defectImages = LoadImages(basePath .. 'Defect/', extension)
-augDefectImages = LoadImages(basePath .. 'DefectAug/', extension)
+if augmentation then
+	augDefectImages = LoadImages(basePath .. 'DefectAug/', extension)
+end
 nonDefectImages = LoadImages(basePath .. 'NonDefect/', extension)
-augNonDefectImages = LoadImages(basePath .. 'NonDefectAug/', extension)
-print('Images loaded. ' .. #defectImages .. ' defectImages, ' .. #augDefectImages .. ' augDefectImages, ' .. #nonDefectImages .. ' nonDefectImages, ' .. #augNonDefectImages .. ' augNonDefectImages')
+if augmentation then
+	augNonDefectImages = LoadImages(basePath .. 'NonDefectAug/', extension)
+end
+print('Images loaded. ') -- .. #defectImages .. ' defectImages, ' .. #augDefectImages .. ' augDefectImages, ' .. #nonDefectImages .. ' nonDefectImages, ' .. #augNonDefectImages .. ' augNonDefectImages')
 
 -- Create empty label tensors
 defectLabels = torch.ones(#defectImages)
-augDefectLabels = torch.ones(#augDefectImages)
+if augmentation then
+	augDefectLabels = torch.ones(#augDefectImages)
+end
 nonDefectLabels = torch.ones(#nonDefectImages):add(1)
-augNonDefectLabels = torch.ones(#augNonDefectImages):add(1)
-print('Labels created. ' .. defectLabels:size(1) .. ' defectLabels, ' .. augDefectLabels:size(1) .. ' augDefectLabels' .. nonDefectLabels:size(1) .. ' nonDefectLabels, ' .. augNonDefectLabels:size(1))
+if augmentation then
+	augNonDefectLabels = torch.ones(#augNonDefectImages):add(1)
+end
+print('Labels created. ') -- .. defectLabels:size(1) .. ' defectLabels, ' .. augDefectLabels:size(1) .. ' augDefectLabels' .. nonDefectLabels:size(1) .. ' nonDefectLabels, ' .. augNonDefectLabels:size(1))
 
 -- Concat images and labels for t7 creation
 allImages = TableConcat(defectImages, nonDefectImages)
-augAllImages = TableConcat(augDefectImages, augNonDefectImages)
+if augmentation then
+	augAllImages = TableConcat(augDefectImages, augNonDefectImages)
+end
 allLabels = torch.cat(defectLabels, nonDefectLabels, 1)
-augAllLabels = torch.cat(augDefectLabels, augNonDefectLabels, 1)
-print('Concat done. ' .. #allImages .. ' allImages, ' .. #augAllImages .. ' augAllImages, ' .. allLabels:size(1) .. ' allLabels, ' .. augAllLabels:size(1) .. ' augAllLabels')
+if augmentation then
+	augAllLabels = torch.cat(augDefectLabels, augNonDefectLabels, 1)
+end
+print('Concat done. ') -- .. #allImages .. ' allImages, ' .. #augAllImages .. ' augAllImages, ' .. allLabels:size(1) .. ' allLabels, ' .. augAllLabels:size(1) .. ' augAllLabels')
 
 -- Scale images to imageSize x imageSize
 for i = 1, #allImages do
-	allImages[i] = image.scale(allImages[i], imageSize, imageSize, 'bilinear')
+	allImages[i] = image.scale(allImages[i], imageSize, imageSize, 'bicubic')
 end
-for i = 1, #augAllImages do
-	augAllImages[i] = image.scale(augAllImages[i], imageSize, imageSize, 'bilinear')
+if augmentation then
+	for i = 1, #augAllImages do
+		augAllImages[i] = image.scale(augAllImages[i], imageSize, imageSize, 'bicubic')
+	end
 end
 print('Image resize done.') 
 
 -- Convert table of images to a tensor
 imgTensor = TableToTensor(allImages)
-augImgTensor = TableToTensor(augAllImages)
+if augmentation then
+	augImgTensor = TableToTensor(augAllImages)
+end
 labelTensor = torch.Tensor(allLabels)
-augLabelTensor = torch.Tensor(augAllLabels)
+if augmentation then
+	augLabelTensor = torch.Tensor(augAllLabels)
+end
 print('Tensor assignment done.')
 
 -- Split data into training and test
 trainData, trainLabels, testData, testLabels = SplitDataset(imgTensor, labelTensor, 0.5)
-trainData = torch.cat(trainData, augImgTensor, 1)
-trainLabels = torch.cat(trainLabels, augLabelTensor, 1)
+if augmentation then
+	trainData = torch.cat(trainData, augImgTensor, 1)
+	trainLabels = torch.cat(trainLabels, augLabelTensor, 1)
+end
 
 trainDataExport = {
 	data = trainData,
@@ -63,7 +85,6 @@ testDataExport = {
 print('Created dataset with ' .. trainData:size(1) .. ' trainingData and ' .. testData:size(1) .. ' testData')
 print('Saving datasets...')
 
-setName = 'defectAndNonDefectAllAug' .. tostring(imageSize)
 torch.save(setName .. '-train.t7', trainDataExport, 'ascii')
 torch.save(setName .. '-test.t7', testDataExport, 'ascii')
 
